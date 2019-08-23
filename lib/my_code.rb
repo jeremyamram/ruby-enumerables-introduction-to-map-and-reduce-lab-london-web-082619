e2fabb2d535ec8960f60a241bf2bb8d752a6fb34
Jeremy Amram <jeremy.j.amram@gmail.com>
2019-08-23 09:51:09 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,18 +1,21 @@
 # My Code here....
 def map_to_negativize(source_array)
-  source_array**-1
+  source_array*-1
 end
 
 def map_to_no_change(source_array)
-  source_array**1 
+  source_array*1 
 end
 
 def map_to_double(source_array)
-  source_array**2
+  source_array*2
 end
 
-def map_to_square
+def map_to_square(source_array)
+  source_array**2 
+end
 
+def reduce_to_total(source_array, starting_point)
 
 
 source_array = [1, 2, 3, -9]
